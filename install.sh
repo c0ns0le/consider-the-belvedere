796a319483504a0b47382bf3b4082eadb39bba7b
scisci <dpriley@ucla.edu>
2015-04-17 11:12:32 -0400

    updating installer

--- install.sh
+++ install.sh
@@ -7,12 +7,15 @@ cat <<EOM >~/Library/LaunchAgents/considerthebelvedere.plist
 <!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
 <plist version="1.0">
 <dict>
-   <key>Label</key>
-   <string>com.considerthebelvedere</string>
-   <key>Program</key>
-   <string>$STARTUP</string>
-   <key>RunAtLoad</key>
-   <true/>
+  <key>Label</key>
+  <string>com.considerthebelvedere</string>
+  <key>Program</key>
+  <array>
+    <string>bash</string>
+    <string>$STARTUP</string>
+  </array>
+  <key>RunAtLoad</key>
+  <true/>
 </dict>
 </plist>
 EOM
---
+++ uninstall.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+rm ~/Library/LaunchAgents/considerthebelvedere.plist
